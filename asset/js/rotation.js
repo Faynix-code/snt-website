62d6da79f7892a83caf2f7e6a5d8168026d2e4b6
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-12-06 21:57:35 +0100

    Le plus gros du travail

---
+++ asset/js/rotation.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".texte", {
+        strings: ["autonome", "intelligent","les choses plus spéciales", "certaines choses plus simple", "les truc plus cooooll" , "fainéant (nan je rigole à peine ) "],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true
+    });
+});
